577fd2239eb5d4f7a90e4754da949e41e2152173
sternsternstern <emil@kern.tk>
2025-11-18 23:17:29 +0100

    organization and started new area scene

--- liquidgame/Assets/Scenes/Areas/area.gd
+++ liquidgame/Assets/Scenes/Areas/area.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 @export var start_point: Node3D
-@export var start_rotation: Vector3
 @export var rotation_lim: Vector2
 @export var area_name: String
 @export_file("*.tscn") var _prev: String

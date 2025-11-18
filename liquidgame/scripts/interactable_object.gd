fb18e336187d2e35562b541aeae75a525742c83e
pane111 <jakob.njh@gmail.com>
2025-11-18 21:54:04 +0100

    Hovering toggle

--- liquidgame/scripts/interactable_object.gd
+++ liquidgame/scripts/interactable_object.gd
@@ -1,9 +1,14 @@
 extends "res://Assets/Scenes/System/interactable.gd"
 
 @export var dialogue: DialogueResource
+@export var hovering = true
 # Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !hovering:
+		$AnimationPlayer.stop()
 func _on_interact():
 	get_node("/root/MainGame").dialogue_anim(dialogue)
-	$AnimationPlayer.current_animation = "clicked"
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer.current_animation = "hover"
+	if hovering:
+		$AnimationPlayer.current_animation = "clicked"
+		await $AnimationPlayer.animation_finished
+		$AnimationPlayer.current_animation = "hover"

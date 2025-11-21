0d0692d4e2f4de51f124681d1f9af7dabc3ba7a9
pane111 <jakob.njh@gmail.com>
2025-11-21 01:18:10 +0100

    Hide npcs with flags

--- liquidgame/Assets/Scenes/npc.gd
+++ liquidgame/Assets/Scenes/npc.gd
@@ -2,15 +2,32 @@ extends "res://Assets/Scenes/System/interactable.gd"
 
 @export var dialogue: DialogueResource
 @export var character: Character
+@export var req_flag: String
+@export var flag_is_string = false # If TRUE, the flag returns a string, eg flag "last_room" = "office", if FALSE, check an int, eg "completed_tutorial" = 1
+@export var flag_val_int: int
+@export var flag_val_string: String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Outline.texture = character.normal_outline
-	$Color.texture = character.normal_color
-	$Shading.texture = character.normal_shading
-	$Shading.modulate = Color("ee82ee")
-	$Outline.billboard = FlagManager.billboarding
-	$Color.billboard = FlagManager.billboarding
-	$Shading.billboard = FlagManager.billboarding
+	var showchar = true
+	if req_flag != "" && req_flag!= null: # Check if the NPC has a required flag
+		if flag_is_string && FlagManager.get_str(req_flag) != flag_val_string: # If flag is a STRING, and the flag in FlagManager does not equal the required value, hide character
+			showchar=false
+		if !flag_is_string && FlagManager.get_int(req_flag) != flag_val_int: # If flag is an INT, and the flag in FlagManager does not equal the required value, hide character
+			showchar=false
+	if !showchar:
+		$Area3D.input_ray_pickable = false
+		$Outline.hide()
+		$Color.hide()
+		$Shading.hide()
+	else:
+		$Outline.texture = character.normal_outline
+		$Color.texture = character.normal_color
+		$Shading.texture = character.normal_shading
+		$Shading.modulate = Color("ee82ee")
+		$Outline.billboard = FlagManager.billboarding
+		$Color.billboard = FlagManager.billboarding
+		$Shading.billboard = FlagManager.billboarding
+	
 	# $Color.material_overlay = character.material
 	
 func _on_interact():
